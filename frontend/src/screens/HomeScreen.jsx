f06ed5a979c94f75804509679407ff5f175d57bc
Hikmatullah <hikmathakim7200@gmail.com>
2023-07-04 23:33:04 +0100

    checking how updates are done on an ubuntu

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from '../components/Hero'
+import Button from 'react-bootstrap/Button';
+
+
+
+
 
 const HomeScreen = () => {
+
+  const [count, setCount] = useState(0)
   return (
     <div>
-        <Hero/>
+        <Hero/> 
+
+        {/* hero is a component that the viewer see first  */}
+
+        <h2>Count {count}</h2>
+        <Button variant="success" onClick={()=>setCount(count+1)}>+</Button>{' '}
+        <Button variant="danger" onClick={()=>setCount(count-1)}>-</Button>{' '}
+
+
+
     </div>
   )
 }
